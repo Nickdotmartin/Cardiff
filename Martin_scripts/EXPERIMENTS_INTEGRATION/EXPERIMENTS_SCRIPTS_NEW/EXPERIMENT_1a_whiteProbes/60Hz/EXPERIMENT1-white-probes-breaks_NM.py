bd436d9143d306e196eb593ad93aacd34a252275
nickdotmartin <nm13850@bristol.ac.uk>
2021-11-08 16:32:11 +0000

    Exp1: Got rid of unused import statements.  Put in idiot checks for correct monitor use. Psychopy_tools: not sure I need this any more, could delete monitor stuff.

--- Martin_scripts/EXPERIMENTS_INTEGRATION/EXPERIMENTS_SCRIPTS_NEW/EXPERIMENT_1a_whiteProbes/60Hz/EXPERIMENT1-white-probes-breaks_NM.py
+++ Martin_scripts/EXPERIMENTS_INTEGRATION/EXPERIMENTS_SCRIPTS_NEW/EXPERIMENT_1a_whiteProbes/60Hz/EXPERIMENT1-white-probes-breaks_NM.py
@@ -1,48 +1,46 @@
 from __future__ import division
-from psychopy import sound, gui, visual, core, data, event, logging, clock, monitors
-# from psychopy.visual import ShapeStim, EnvelopeGrating, Circle
-from psychopy.visual.shape import ShapeStim
-from psychopy.visual.secondorder import EnvelopeGrating
-from psychopy.visual.circle import Circle
-
-import psychopy
+from psychopy import gui, visual, core, data, event, logging, monitors
+from psychopy import __version__ as psychopy_version
+# import psychopy
 import os
-import numpy
-from numpy import (arcsin, arccos, arctan, sin, cos, tan, pi, average, sqrt, std, deg2rad, rad2deg)
+from numpy import deg2rad
 from numpy.random import shuffle
 import random
 import copy
-import time
 from datetime import datetime
 from math import *
-from scipy.optimize import fsolve
 
 from kestenSTmaxVal import Staircase
 
-logging.console.setLevel(logging.CRITICAL)
+
+# sets psychoPy to only log critical messages
+# logging.console.setLevel(logging.CRITICAL)
 
 # Ensure that relative paths start from the same directory as this script
 _thisDir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(_thisDir)
 
+# Monitor config from monitor centre
+monitor_name = 'HP_24uh'  # 'NickMac' 'asus_cal' 'Asus_VG24' 'HP_24uh' # gamma set at 2.1
+display_number = 1  # 0 indexed, 1 for external display
+
+
 
 # Store info about the experiment session
-# psychopyVersion = psychopy.__version__
 expName = 'integration-EXP1'  # from the Builder filename that created this script
 
-# todo: change dict keys once I've looked at analysis
-expInfo = {'1. Participant': 'test',
-           '2. Probe duration in frames at 240hz': '2',
-           # '3. fps':['60'],
-           '3. fps': 60,
-           '4. ISI duration in frame': ['0', '2', '4', '6', '9', '12', '24', '-1'],
+# todo: change dict keys (ISI str to int?) once I've looked at analysis
+expInfo = {'1. Participant': 'testnm',
+           '2. Probe duration in frames at 240hz': 2,
+           '3. fps': [60, 144, 240],
+           '4. ISI duration in frame': [0, 2, 4, 6, 9, 12, 24, -1],
            '5. Probe orientation': ['tangent'],
            '6. Probe size': ['5pixels', '6pixels', '3pixels'],
-           '7. Background lum in percent of maxLum': '20',
+           '7. Background lum in percent of maxLum': 20,
            '8. Red filter': ['no', 'yes']}
 
 
-## GUI
+# GUI
 dlg = gui.DlgFromDict(dictionary=expInfo, title=expName)
 if not dlg.OK:
     core.quit()  # user pressed cancel
@@ -54,10 +52,11 @@ expInfo['date'] = datetime.now().strftime("%d/%m/%Y")
 participant_name = expInfo['1. Participant']
 trial_number = 25
 probe_duration = int(expInfo['2. Probe duration in frames at 240hz'])
-probe_ecc = 4  # int((expInfo['6. Probe eccentricity in deg']))
-# fps = float(expInfo['3. fps'])
-fps = expInfo['3. fps']
+probe_ecc = 4
+fps = int(expInfo['3. fps'])
 orientation = expInfo['5. Probe orientation']
+# ISI durations, -1 correspond to simultaneous probes
+ISI = int(expInfo['4. ISI duration in frame'])
 
 
 # VARIABLES
@@ -65,20 +64,17 @@ orientation = expInfo['5. Probe orientation']
 # 99 values for single probe condition
 separations = [18, 18, 6, 6, 3, 3, 2, 2, 1, 1, 0, 0, 99, 99]
 
-# ISI durations, -1 correspond to simultaneous probes
-ISI = int(expInfo['4. ISI duration in frame'])
+
 
 # FILENAME
-# todo: look at filenames - are there any sq brackets?  
-#  also remove redundant brackets round participant name
-filename = (_thisDir + os.sep + '%s' % (participant_name) +
-            os.sep + ('ISI_' + expInfo['4. ISI duration in frame'] + 
-            '_probeDur' + expInfo['2. Probe duration in frames at 240hz']) +
-            os.sep + participant_name)
-# filename = f'{_thisDir}{os.sep}'
+filename = f'{_thisDir}{os.sep}' \
+           f'{participant_name}{os.sep}' \
+           f'ISI_{ISI}' \
+           f'_probeDur{probe_duration}' \
+           f'{os.sep}{participant_name}'
 
 # Experiment Handler
-thisExp = data.ExperimentHandler(name=expName, version='',
+thisExp = data.ExperimentHandler(name=expName, version=psychopy_version,
                                  extraInfo=expInfo, runtimeInfo=None,
                                  savePickle=None, saveWideText=True,
                                  dataFileName=filename)
@@ -97,53 +93,70 @@ maxColor255 = 255
 minColor255 = 0
 maxColor1 = 1
 minColor1 = -1
-# todo: check these double parenthesis
-bgLumP = int((expInfo['7. Background lum in percent of maxLum']))
+bgLumP = int(expInfo['7. Background lum in percent of maxLum'])
 bgLum = maxLum * bgLumP / 100
 bgColor255 = bgLum * LumColor255Factor
 
-# todo: figure out how to access win size property
-# print('get mon size')
-# win_size = visual.Window.size()
-# print(win_size)
-# print(type(win_size))
-# this_window = visual.Window
-# window_size = this_window.size
-# print(window_size)
-# screenWidth=visual.Window.size[0]
-# screenHeight=visual.Window.size[1]
-# print(f'width: {screenWidth}, height: {screenHeight}')
-
 
 # MONITOR SPEC
-widthPix = 1440  # 1280
-heightPix = 900  # 800
-monitorwidth = 30.41  # 32.512  # monitor width in cm
-viewdist = 57.3  # viewing distance in cm
+thisMon = monitors.Monitor(monitor_name)
+this_width = thisMon.getWidth()
+mon_dict = {'mon_name': monitor_name,
+            'width': thisMon.getWidth(),
+            'size': thisMon.getSizePix(),
+            'dist': thisMon.getDistance(),
+            'notes': thisMon.getNotes()
+            }
+print(f"mon_dict: {mon_dict}")
+
+widthPix = mon_dict['size'][0]  # 1440  # 1280
+heightPix = mon_dict['size'][1]  # 900  # 800
+monitorwidth = mon_dict['width']  # 30.41  # 32.512  # monitor width in cm
+viewdist = mon_dict['dist']  # 57.3  # viewing distance in cm
 viewdistPix = widthPix/monitorwidth*viewdist
-monitorname = 'NickMac'  # 'asus_cal'  # gamma set at 2.1
-mon = monitors.Monitor(monitorname, width=monitorwidth, distance=viewdist)
-# todo: check these double parenthesis
+mon = monitors.Monitor(monitor_name, width=monitorwidth, distance=viewdist)
 mon.setSizePix((widthPix, heightPix))
 mon.save()
+
 
 # WINDOW SPEC
 win = visual.Window(monitor=mon, size=(widthPix, heightPix),
                     colorSpace='rgb255', color=bgColor255,
-                    # I've added winType to make it work on my mac
-                    winType='pyglet',
-
-                    # pos gives position of top-left of screen
-                    pos=[1, -1],
+                    winType='pyglet',  # I've added this to make it work on pycharm/mac
+                    pos=[1, -1],  # pos gives position of top-left of screen
                     units='pix',
-                    screen=1,
+                    screen=display_number,
                     allowGUI=False,
                     fullscr=None
                     )
 
+# check correct monitor details (fps, size) have been accessed.
+print(win.monitor.name, win.monitor.getSizePix())
+actualFrameRate = int(win.getActualFrameRate())
+if fps in list(range(actualFrameRate-2, actualFrameRate+2)):
+    print("fps matches actual frame rate")
+else:
+    # if values don't match, quit experiment
+    print(f"fps ({fps}) does not match actual frame rate ({actualFrameRate})")
+    core.quit()
+
+
+actual_size = win.size
+if list(mon_dict['size']) == list(actual_size):
+    print(f"monitor is expected size")
+elif list(mon_dict['size']) == list(actual_size/2):
+    print(f"actual size is double expected size - Its ok, just a mac retina display bug.")
+else:
+    print(f"Display size does not match expected size from montior centre")
+    # check sizes seems unreliable,
+    # it returns different values for same screen if different mon_names are used!
+    check_sizes = win._checkMatchingSizes(mon_dict['size'], actual_size)
+    print(check_sizes)
+    core.quit()
+
+
 # ELEMENTS
 # fixation bull eye
-# todo: why don't I have Circle, `envelopeGrating or ShapeSTIM?
 fixation = visual.Circle(win, radius=2, units='pix',
                          lineColor='white', fillColor='black')
 
@@ -153,17 +166,21 @@ if expInfo['8. Red filter'] == 'yes':
     redfilter = -1
 else:
     redfilter = 1
-# probre sizes choice
-# todo: probvert can be undefined - e.g, iuf statements don't capture all possibilities.
-if expInfo['6. Probe size'] == '6pixels':
-    probeVert = [(0, 0), (1, 0), (1, 1), (2, 1), (2, -2),
-                 (-1, -2), (-1, -1), (0, -1)]  # 6 pixels
-elif expInfo['6. Probe size'] == '5pixels':
-    probeVert = [(0, 0), (1, 0), (1, 1), (2, 1), (2, -1), (1, -1),
-                 (1, -2), (-1, -2), (-1, -1), (0, -1)]  # 5 pixels
-elif expInfo['6. Probe size'] == '3pixels':
+
+# probe sizes choice
+if expInfo['6. Probe size'] == '6pixels':  # 6 pixels
+    probeVert = [(0, 0), (1, 0), (1, 1), (2, 1),
+                 (2, -2), (-1, -2), (-1, -1), (0, -1)]
+
+elif expInfo['6. Probe size'] == '3pixels':  # 3 pixels
     probeVert = [(0, 0), (1, 0), (1, 1), (2, 1), (2, 0), (1, 0), (1, -1),
-                 (0, -1), (0, -2), (-1, -2), (-1, -2), (-1, -1), (0, -1)]  # 3 pixels
+                 (0, -1), (0, -2), (-1, -2), (-1, -2), (-1, -1), (0, -1)]
+
+else:  # 5 pixels
+    # default setting is expInfo['6. Probe size'] == '5pixels':
+    expInfo['6. Probe size'] = '5pixels'
+    probeVert = [(0, 0), (1, 0), (1, 1), (2, 1), (2, -1), (1, -1),
+                 (1, -2), (-1, -2), (-1, -1), (0, -1)]
 
 probe1 = visual.ShapeStim(win, vertices=probeVert, fillColor=(1.0, -1.0, 1.0),
                           lineWidth=0, opacity=1, size=1, interpolate=False)
@@ -175,13 +192,12 @@ myMouse = event.Mouse(visible=False)
 
 # ------------------------------------------------------------------- INSTRUCTION
 # ------------------------------------------------------------------- INSTRUCTION
-# todo: add key for previous trial.
 instructions = visual.TextStim(win=win, name='instructions',
                                text="[q] or [4] top-left\n "
                                     "[w] or [5] top-right\n "
                                     "[a] or [1] bottom-left\n "
                                     "[s] or [2] bottom-right \n\n "
-                                    "redo the previous trial \n\n"
+                                    "[r] or [9] to redo the previous trial \n\n"
                                     "[Space bar] to start",
                                font='Arial', pos=[0, 0], height=20, ori=0, color=[255, 255, 255],
                                colorSpace='rgb255', opacity=1, languageStyle='LTR', depth=0.0)
@@ -195,6 +211,7 @@ breaks = visual.TextStim(win=win, name='breaks',
 while not event.getKeys():
     instructions.draw()
     win.flip()
+
 # ------------------------------------------------------------------- STAIRCASE
 # ------------------------------------------------------------------- STAIRCASE
 total_nTrials = 0
@@ -247,7 +264,6 @@ for trialN in range(expInfo['nTrials']):
 
         # PROBE LOCATION
         corner = random.choice([45, 135, 225, 315])
-        # todo: check these double parenthesis
         x_prob = round((tan(deg2rad(probe_ecc))*viewdistPix)/sqrt(2))
         y_prob = round((tan(deg2rad(probe_ecc))*viewdistPix)/sqrt(2))
 
@@ -261,12 +277,11 @@ for trialN in range(expInfo['nTrials']):
                 if target_jump == 1:  # CCW
                     probe1.ori = 0
                     probe2.ori = 180
-                    # todo: are these brackets needed?
-                    probe2.pos = [p1_x - (sep)+1, p1_y + (sep)]
+                    probe2.pos = [p1_x - sep+1, p1_y + sep]
                 elif target_jump == -1:  # CW
                     probe1.ori = 180
                     probe2.ori = 0
-                    probe2.pos = [p1_x + (sep)-1, p1_y - (sep)]
+                    probe2.pos = [p1_x + sep-1, p1_y - sep]
                 elif target_jump == 9:
                     probe1.ori = random.choice([0, 180])
         elif corner == 135:
@@ -276,11 +291,11 @@ for trialN in range(expInfo['nTrials']):
                 if target_jump == 1:  # CCW
                     probe1.ori = 90
                     probe2.ori = 270
-                    probe2.pos = [p1_x + (sep)-1, p1_y + (sep)]
+                    probe2.pos = [p1_x + sep-1, p1_y + sep]
                 elif target_jump == -1:  # CW
                     probe1.ori = 270
                     probe2.ori = 90
-                    probe2.pos = [p1_x - (sep)+1, p1_y - (sep)]
+                    probe2.pos = [p1_x - sep+1, p1_y - sep]
                 elif target_jump == 9:
                     probe1.ori = random.choice([90, 270])
         elif corner == 225:
@@ -290,25 +305,26 @@ for trialN in range(expInfo['nTrials']):
                 if target_jump == 1:  # CCW
                     probe1.ori = 180
                     probe2.ori = 0
-                    probe2.pos = [p1_x + (sep)-1, p1_y - (sep)]
+                    probe2.pos = [p1_x + sep-1, p1_y - sep]
                 elif target_jump == -1:  # CW
                     probe1.ori = 0
                     probe2.ori = 180
-                    probe2.pos = [p1_x - (sep)+1, p1_y + (sep)]
+                    probe2.pos = [p1_x - sep+1, p1_y + sep]
                 elif target_jump == 9:
                     probe1.ori = random.choice([0, 180])
-        elif corner == 315:
+        else:
+            corner = 315
             p1_x = x_prob * 1
             p1_y = y_prob * -1
             if orientation == 'tangent':
                 if target_jump == 1:  # CCW
                     probe1.ori = 270
                     probe2.ori = 90
-                    probe2.pos = [p1_x - (sep)+1, p1_y - (sep)]
+                    probe2.pos = [p1_x - sep+1, p1_y - sep]
                 elif target_jump == -1:  # CW
                     probe1.ori = 90
                     probe2.ori = 270
-                    probe2.pos = [p1_x + (sep)-1, p1_y + (sep)]
+                    probe2.pos = [p1_x + sep-1, p1_y + sep]
                 elif target_jump == 9:
                     probe1.ori = random.choice([90, 270])
 
@@ -380,30 +396,29 @@ for trialN in range(expInfo['nTrials']):
                     if len(theseKeys) > 0:  # at least one key was pressed
                         resp.keys = theseKeys[-1]  # just the last key pressed
                         resp.rt = resp.clock.getTime()
-
                         if corner == 45:
-                            if (resp.keys == str('w')) or (resp.keys == 'w') or (resp.keys == 'num_5'):
+                            if (resp.keys == 'w') or (resp.keys == 'num_5'):
                                 resp.corr = 1
                             else:
                                 resp.corr = 0
                             repeat = False
                             continueRoutine = False
                         elif corner == 135:
-                            if (resp.keys == str('q')) or (resp.keys == 'q') or (resp.keys == 'num_4'):
+                            if (resp.keys == 'q') or (resp.keys == 'num_4'):
                                 resp.corr = 1
                             else:
                                 resp.corr = 0
                             repeat = False
                             continueRoutine = False
                         elif corner == 225:
-                            if (resp.keys == str('a')) or (resp.keys == 'a') or (resp.keys == 'num_1'):
+                            if (resp.keys == 'a') or (resp.keys == 'num_1'):
                                 resp.corr = 1
                             else:
                                 resp.corr = 0
                             repeat = False
                             continueRoutine = False
                         elif corner == 315:
-                            if (resp.keys == str('s')) or (resp.keys == 's') or (resp.keys == 'num_2'):
+                            if (resp.keys == 's') or (resp.keys == 'num_2'):
                                 resp.corr = 1
                             else:
                                 resp.corr = 0
@@ -413,6 +428,7 @@ for trialN in range(expInfo['nTrials']):
                 # check for quit
                 if event.getKeys(keyList=["escape"]):
                     core.quit()
+
                 # redo the trial if i think i made a mistake
                 if event.getKeys(keyList=["r"]) or event.getKeys(keyList=['num_9']):
                     repeat = True
@@ -422,6 +438,7 @@ for trialN in range(expInfo['nTrials']):
                 # refresh the screen
                 if continueRoutine:
                     win.flip()
+
 
         thisExp.addData('stair', stairNum)
         thisExp.addData('probe_jump', target_jump)
--- PsychoPy_tools.py
+++ PsychoPy_tools.py
@@ -1,116 +1,123 @@
 import psychopy
 from psychopy import monitors, info
-
 '''
+
 Page has a set of tools to use for psychoPy experiments
 
 '''
 
 print(psychopy.__version__)
 
-def select_monitor(mon_name='NickMac'):
-    """
-    Function to adjust the refresh rate and size for the relevant monitor.
-    Input the monitor name
-
-
-    :return: a dict with relevant values (size, refresh etc)
-    """
-
-    mon_dict = {'mon_name': 'NickMac',
-                'width': 30.41,
-                'size': [2560.0, 1600.0],
-                'dist': 60.0,
-                'notes': 'MacBook Air 13inch, 60Hz'}
-
-    if mon_name == 'Asus_VG24':
-        mon_dict = {'mon_name': 'Asus_VG24',
-                    'width': 53.13,
-                    'size': [1920.0, 1080.0],
-                    'dist': 75.0,
-                    'notes': 'Asus monitor in room 2.18, 144 Hz refresh'}
-
-    elif mon_name == 'asus_cal':
-        mon_dict = {'mon_name': 'asus_cal',
-                    'width': 32.512,
-                    'size': [1280, 800],
-                    'dist': 57.3,
-                    'notes': None}
-
-    elif mon_name == 'testMontior':
-        mon_dict = {'mon_name': 'testMonitor',
-                    'width': 30,
-                    'size': [1024, 768],
-                    'dist': 57,
-                    'notes': 'default (not very useful) monitor'}
-
-
-
-    # # add in stuff for monitor in 2.13d
-    # elif mon_name == 'fancy 2.13d':
-    #     mon_dict['refresh'] = 200
-
-    return mon_dict
+# def select_monitor(mon_name='NickMac'):
+#     """
+#     Function to adjust the refresh rate and size for the relevant monitor.
+#     Input the monitor name
+#
+#
+#     :return: a dict with relevant values (size, refresh etc)
+#     """
+#
+#     mon_dict = {'mon_name': 'NickMac',
+#                 'width': 30.41,
+#                 'size': [1440, 900],
+#                 'dist': 60.0,
+#                 'notes': 'MacBook Air 13inch, 60Hz'}
+#
+#     if mon_name == 'Asus_VG24':
+#         mon_dict = {'mon_name': 'Asus_VG24',
+#                     'width': 53.13,
+#                     'size': [1920.0, 1080.0],
+#                     'dist': 75.0,
+#                     'notes': 'Asus monitor in room 2.18, 144 Hz refresh'}
+#
+#     elif mon_name == 'asus_cal':
+#         mon_dict = {'mon_name': 'asus_cal',
+#                     'width': 32.512,
+#                     'size': [1280, 800],
+#                     'dist': 57.3,
+#                     'notes': None}
+#
+#     elif mon_name == 'testMontior':
+#         mon_dict = {'mon_name': 'testMonitor',
+#                     'width': 30,
+#                     'size': [1024, 768],
+#                     'dist': 57,
+#                     'notes': 'default (not very useful) monitor'}
+#
+#
+#
+#     # # add in stuff for monitor in 2.13d
+#     # elif mon_name == 'fancy 2.13d':
+#     #     mon_dict['refresh'] = 200
+#
+#     return mon_dict
 
 
 # print(select_monitor('Asus_VG24'))
 
-runtime_info = info.RunTimeInfo(refreshTest=True)
-print(runtime_info)
+# runtime_info = info.RunTimeInfo(refreshTest=False)
+# print(runtime_info)
 
-names = monitors.getAllMonitors()
-for thisName in names:
-    thisMon = monitors.Monitor(thisName)
-    this_width = thisMon.getWidth()
-    mon_dict = {'mon_name': thisName,
-                'width': thisMon.getWidth(),
-                'size': thisMon.getSizePix(),
-                'dist': thisMon.getDistance(),
-                'notes': thisMon.getNotes()
-                }
+this_mon = monitors.Monitor()
+print(this_mon)
 
-    print(thisName)
-    # # search for refresh rate in notes
-    if mon_dict['notes'] is not None:
-        notes = mon_dict['notes'].lower()
-
-        # check for 'hz', and that it only occurs once
-        if "hz" in notes:
-            if notes.count('hz') == 1:
-                find_hz = notes.find("hz")
-
-                # if hz are last two characters, don't use -1
-                if find_hz == -1:
-                    # use -2 because there are two characters in 'hz'
-                    find_hz = len(notes) - 2
-
-                # strip away text from 'hz' onwards
-                slice = notes[:find_hz]
-
-                # remove space between int and 'hz' if present
-                if slice[-1] == " ":
-                    slice = slice[:-1]
-
-                # to find how many ints there are, look for white space
-                whitespaces = []
-                for index, character in enumerate(slice):
-                    if character == " ":
-                        whitespaces.append(index)
-
-                # slice from space before 'hz' to just leave numbers
-                last_space = whitespaces[-1]
-                convert_this = slice[last_space:]
-                converted = int(convert_this)
-
-                mon_dict['refresh'] = converted
-
-        #     else:
-        #         print('There are multiple refeences to Hz in notes.  '
-        #               'This script only works if there is a single reference.')
-        # else:
-        #     print("refresh rate info ('Hz') not found")
-
-                print(mon_dict)
+# names = monitors.getAllMonitors()
+# for thisName in names:
+#     thisMon = monitors.Monitor(thisName)
+#     this_width = thisMon.getWidth()
+#     mon_dict = {'mon_name': thisName,
+#                 'width': thisMon.getWidth(),
+#                 'size': thisMon.getSizePix(),
+#                 'dist': thisMon.getDistance(),
+#                 'notes': thisMon.getNotes()
+#                 }
+#
+#     print(thisName)
+#     # # search for refresh rate in notes
+#     if mon_dict['notes'] is not None:
+#         notes = mon_dict['notes'].lower()
+#
+#         # check for 'hz', and that it only occurs once
+#         if "hz" in notes:
+#             if notes.count('hz') == 1:
+#                 find_hz = notes.find("hz")
+#
+#                 # if hz are last two characters, don't use -1
+#                 if find_hz == -1:
+#                     # use -2 because there are two characters in 'hz'
+#                     find_hz = len(notes) - 2
+#
+#                 # strip away text from 'hz' onwards
+#                 slice = notes[:find_hz]
+#
+#                 # remove space between int and 'hz' if present
+#                 if slice[-1] == " ":
+#                     slice = slice[:-1]
+#
+#                 # to find how many ints there are, look for white space
+#                 whitespaces = []
+#                 for index, character in enumerate(slice):
+#                     if character == " ":
+#                         whitespaces.append(index)
+#
+#                 # if there are no whitespaces, just use this slice
+#                 if len(whitespaces) == 0:
+#                     convert_this = slice
+#                 else:
+#                     # slice from space before 'hz' to just leave numbers
+#                     last_space = whitespaces[-1]
+#                     convert_this = slice[last_space:]
+#                 converted = int(convert_this)
+#
+#                 mon_dict['refresh'] = converted
+#
+#         #     else:
+#         #         print('There are multiple refeences to Hz in notes.  '
+#         #               'This script only works if there is a single reference.')
+#         # else:
+#         #     print("refresh rate info ('Hz') not found")
+#
+#                 print(mon_dict)
 
 
 
