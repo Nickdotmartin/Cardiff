f13d08b2af8ec4763c91f3b74e8f3b9d7fe7d1ef
Nickdotmartin <Nickdotmartin@googlemail.com>
2022-05-17 12:08:06 +0100

    Monitor_response_time_stim.py: deleted commented out code to tidy things up.

--- Nick_scripts/Monitor_response_time_stim.py
+++ Nick_scripts/Monitor_response_time_stim.py
@@ -14,24 +14,6 @@ from PsychoPy_tools import check_correct_monitor
 from kestenSTmaxVal import Staircase
 
 
-def getResponse(question):
-    """
-    I wrote this function to avoid any incorrect responses being inputted.
-    Will accept 1, 2 or 9 in response to any question.  If a key other than
-    these is entered, it will keep asking until it gets a valid response.
-
-    :param question:  Any string, which will appear on screen
-    :return: a valid response (1, 2 or 9)
-    """
-    respond_1_2_or_9 = input(question)
-    if respond_1_2_or_9 == "1" or respond_1_2_or_9 == "2" or respond_1_2_or_9 == "9":
-        return int(respond_1_2_or_9)  # converted to int
-    else:
-        return getResponse(f'Try-again: {question}')
-
-
-# sets psychoPy to only log critical messages
-# logging.console.setLevel(logging.CRITICAL)
 
 # Ensure that relative paths start from the same directory as this script
 _thisDir = os.path.dirname(os.path.abspath(__file__))
@@ -56,11 +38,9 @@ All stim at max luminance, recordings for 45 frames/187.5ms
 # Store info about the experiment session
 expName = 'monitor_resp_times_stim'  # from the Builder filename that created this script
 
-# todo: change dict keys (ISI str to int?) once I've looked at analysis
 expInfo = {'1. Participant': 'mon_resp_time_stim',
            '2. Probe duration in frames at 240hz': 2,
            '3. fps': [60, 144, 240],
-           # '4. ISI duration in frame': [0, 2, 4, 6, 9, 12, 24, -1],
            '5. Probe orientation': ['tangent'],
            '6. Probe size': ['5pixels', '6pixels', '3pixels'],
            '7. Background lum in percent of maxLum': 20,
@@ -83,9 +63,6 @@ probe_duration = int(expInfo['2. Probe duration in frames at 240hz'])
 probe_ecc = 4
 fps = int(expInfo['3. fps'])
 orientation = expInfo['5. Probe orientation']
-# ISI durations, -1 correspond to simultaneous probes
-# todo: this will vary - e.g., test several ISIs
-# ISI = int(expInfo['4. ISI duration in frame'])
 
 
 # VARIABLES
@@ -110,17 +87,6 @@ cond_dict = {i: {'sep': sep, 'isi': isi} for (i, sep, isi) in zip(stairs, sep_li
 #     print(this_cond['sep'])
 
 
-# # FILENAME
-# filename = f'{_thisDir}{os.sep}' \
-#            f'{participant_name}{os.sep}' \
-#            f'mon_resp_time_stim'
-#
-# # Experiment Handler
-# thisExp = data.ExperimentHandler(name=expName, version=psychopy_version,
-#                                  extraInfo=expInfo, runtimeInfo=None,
-#                                  savePickle=None, saveWideText=False,
-#                                  dataFileName=filename)
-#
 # COLORS AND LUMINANCE
 # Lum to Color255
 LumColor255Factor = 2.39538706913372
@@ -172,64 +138,11 @@ win = visual.Window(monitor=mon, size=(widthPix, heightPix),
                     fullscr=False,
                     )
 
-# print(f"check win.size: {win.size}")
-# widthPix = widthPix/2
-# heightPix = heightPix/2
-# print(f"widthPix: {widthPix}, hight: {heightPix}")
-# widthPix, heightPix = win.size
-# print(f"check win.size: {win.size}")
-#
-#
-# print(f"type(win.getActualFrameRate()): {type(win.getActualFrameRate())} {win.getActualFrameRate()}")
-# print(f"check win.size: {win.size}")
-# check_correct_monitor(monitor_name=monitor_name,
-#                       actual_size=win.size,
-#                       actual_fps=win.getActualFrameRate(),
-#                       verbose=True)
-#
-# # check correct monitor details (fps, size) have been accessed.
-# print(win.monitor.name, win.monitor.getSizePix())
-# actualFrameRate = int(win.getActualFrameRate())
-#
-# print(f"actual_size: {type(win.monitor.getSizePix())} {win.monitor.getSizePix()}")
-# print(f"actual fps: {type(win.getActualFrameRate())} {win.getActualFrameRate()}")
-#
-# if fps in list(range(actualFrameRate-2, actualFrameRate+2)):
-#     print("fps matches actual frame rate")
-# else:
-#     # if values don't match, quit experiment
-#     print(f"fps ({fps}) does not match actual frame rate ({actualFrameRate})")
-#     core.quit()
-#
-#
-# # actual_size = win.size
-# actual_size = win.monitor.getSizePix()
-# print(f"idiot check. I think I should use win.monitor.getSizePix() {win.monitor.getSizePix()}.\n"
-#       f"currently using win.size {win.size}")
-#
-#
-# if list(mon_dict['size']) == list(actual_size):
-#     print(f"monitor is expected size")
-# elif list(mon_dict['size']) == list(actual_size/2):
-#     print(f"actual size is double expected size - Its ok, just a mac retina display bug.")
-# else:
-#     print(f"Display size does not match expected size from montior centre")
-#     # check sizes seems unreliable,
-#     # it returns different values for same screen if different mon_names are used!
-#     check_sizes = win._checkMatchingSizes(mon_dict['size'], actual_size)
-#     print(check_sizes)
-#     core.quit()
-#
-# # check sizes seems unreliable,
-# print(f"double checking win._checkMatchingSizes({mon_dict['size']}, {actual_size})")
-# # it returns different values for same screen if different mon_names are used!
-# check_sizes = win._checkMatchingSizes(mon_dict['size'], actual_size)
-# print(check_sizes)
+print(f"type(win.getActualFrameRate()): {type(win.getActualFrameRate())} {win.getActualFrameRate()}")
 
 
 # ELEMENTS
 # fixation bull eye
-# todo: change fixation to trails counter?
 fixation = visual.Circle(win, radius=2, units='pix',
                          lineColor='white', fillColor='black')
 
@@ -261,24 +174,24 @@ probe2 = visual.ShapeStim(win, vertices=probeVert, fillColor=[-1.0, 1.0, -1.0],
                           lineWidth=0, opacity=1, size=1, interpolate=False)
 
 # MOUSE - hide cursor
-# myMouse = event.Mouse(visible=False)
-#
-# # INSTRUCTION
-# instructions = visual.TextStim(win=win, name='instructions',
-#                                text="[q] or [4] top-left\n "
-#                                     "[w] or [5] top-right\n "
-#                                     "[a] or [1] bottom-left\n "
-#                                     "[s] or [2] bottom-right \n\n "
-#                                     "[r] or [9] to redo the previous trial \n\n"
-#                                     "[Space bar] to start",
-#                                font='Arial', pos=[0, 0], height=20, ori=0,
-#                                color=[255, 255, 255], colorSpace='rgb255',
-#                                opacity=1, languageStyle='LTR', depth=0.0)
-#
-#
-# while not event.getKeys():
-#     instructions.draw()
-#     win.flip()
+myMouse = event.Mouse(visible=False)
+
+# INSTRUCTION
+instructions = visual.TextStim(win=win, name='instructions',
+                               text="[q] or [4] top-left\n "
+                                    "[w] or [5] top-right\n "
+                                    "[a] or [1] bottom-left\n "
+                                    "[s] or [2] bottom-right \n\n "
+                                    "[r] or [9] to redo the previous trial \n\n"
+                                    "[Space bar] to start",
+                               font='Arial', pos=[0, 0], height=20, ori=0,
+                               color=[255, 255, 255], colorSpace='rgb255',
+                               opacity=1, languageStyle='LTR', depth=0.0)
+
+
+while not event.getKeys():
+    instructions.draw()
+    win.flip()
 
 # Trial counter
 trials_counter = visual.TextStim(win=win, name='trials_counter', text="???",
@@ -298,46 +211,12 @@ breaks = visual.TextStim(win=win, name='breaks',
                          font='Arial', pos=[0, 0], height=20, ori=0, color=[255, 255, 255],
                          colorSpace='rgb255', opacity=1, languageStyle='LTR', depth=0.0)
 
-#
-# # STAIRCASE
-# total_nTrials = 0
-# # todo: change from list(range(1, 15)) to list(range(1, len(separations)))
-# expInfo['startPoints'] = list(range(1, 15))  # 14 staircases (14 conditions)
-# expInfo['nTrials'] = trial_number
-#
-# stairStart = maxLum
-# miniVal = bgLum
-# maxiVal = maxLum
-#
-# stairs = []
-# for thisStart in expInfo['startPoints']:
-# 
-#     thisInfo = copy.copy(expInfo)
-#     thisInfo['thisStart'] = thisStart
-# 
-#     this_cond = Staircase(name='trials',
-#                           type='simple',
-#                           value=stairStart,
-#                           C=stairStart*0.6,  # typically 60% of reference stimulus
-#                           minRevs=3,
-#                           minTrials=trial_number,
-#                           minVal=miniVal,
-#                           maxVal=maxiVal,
-#                           targetThresh=0.75,  # changed this from prev versions
-#                           extraInfo=thisInfo)
-#     stairs.append(this_cond)
 
-
-# EXPERIMENT
-# for trialN in range(expInfo['nTrials']):
-#     shuffle(stairs)
 
 n_conds = len(sep_list)
 this_cond = 0
-responseList = []
 
 # for this_cond, cond_vals in cond_dict.items():
-# while len(responseList) < n_conds:
 while this_cond < n_conds:
 
     cond_vals = cond_dict[this_cond]
@@ -347,21 +226,15 @@ while this_cond < n_conds:
 
 
     target_jump = 1  # random.choice([1, -1])
-    # stairNum = this_cond.extraInfo['thisStart']
-    # staircase varied probeLum
+
     probeLum = maxLum  # this_cond.next()
     probeColor255 = probeLum * LumColor255Factor
     probeColor1 = (probeColor255 * Color255Color1Factor) - 1
 
-    # print(f"\ntrialN: {trialN} this_cond: {this_cond} stairNum: {stairNum}\nprobeLum: {probeLum}")
-
-    # total_nTrials = total_nTrials + 1
 
     # Black or White
     probe1.color = [probeColor1, probeColor1*redfilter, probeColor1*redfilter]
     probe2.color = [probeColor1, probeColor1*redfilter, probeColor1*redfilter]
-
-    # print(f"\nbgLum: {bgLum} bgColor255: {bgColor255} win.colorSpace: {win.colorSpace}")
 
 
     # PROBE LOCATION
@@ -435,19 +308,12 @@ while this_cond < n_conds:
 
     # timing in frames
     # if ISI >= 0:
-    # todo: change t_interval_1 and 2 to t_probe_1 and 2?
     t_fixation = 1 * fps
     t_interval_1 = t_fixation + probe_duration
     t_ISI = t_interval_1 + ISI
     t_interval_2 = t_ISI + probe_duration
     # I presume this means almost unlimited time to respond?
-    t_response = t_interval_2 + 10000*fps
-
-    # print(f't_fixation: {t_fixation}')
-    # print(f't_interval_1: {t_interval_1}')
-    # print(f't_ISI: {t_ISI}')
-    # print(f't_interval_2: {t_interval_2}')
-    # print(f't_response: {t_response}')
+    t_response = t_interval_2 + 100000*fps
 
 
     # repeat the trial if [r] has been pressed
@@ -510,25 +376,6 @@ while this_cond < n_conds:
                 fixation.draw()
                 trials_counter.draw()
 
-                #
-                # # # # # #
-                # response = getResponse(f"Did it record? yes [1] or no [9]\n")
-                #
-                # if response == 9:
-                #     responseList.pop()  # removed the last item in the list
-                #     print(f" Trial:{this_cond}, sep: {cond_vals['sep']}, isi: {cond_vals['isi']}. re-do trial")
-                #     # this_cond -= 1  # go back one trial number
-                #     # this_cond = this_cond  # go back one trial number
-                #
-                # else:
-                #     # if not re-do trial, it must be a valid response (1 or 2)
-                #     # dataToSave = f"{thisStair} {currentSize1} {response}\n"  # old txt version
-                #
-                #     dataToSave = response
-                #     # I just append data to a list, and write the file at the end.
-                #     responseList.append(dataToSave)
-                #     this_cond += 1
-                # # # # # #
 
                 # ANSWER
                 resp = event.BuilderKeyResponse()
@@ -577,17 +424,4 @@ while this_cond < n_conds:
                 win.flip()
 
 
-    # thisExp.addData('stair', stairNum)
-    # thisExp.addData('probe_jump', target_jump)
-    # thisExp.addData('probeColor1', probeColor1)
-    # thisExp.addData('probeColor255', probeColor255)
-    # thisExp.addData('probeLum', probeLum)
-    # thisExp.addData('trial_response', resp.corr)
-    # thisExp.addData('corner', corner)
-    # thisExp.addData('probe_ecc', probe_ecc)
-    # thisExp.addData('resp.rt', resp.rt)
-    # thisExp.addData('total_nTrials', total_nTrials)
-    # thisExp.addData('orientation', orientation)
-    # thisExp.nextEntry()
-    #
-    # this_cond.newValue(resp.corr)   # so that the staircase adjusts itself
+print('testing finished')
