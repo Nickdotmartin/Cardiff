f3295698bc380e2867b35896099e9b3c0503504e
nickdotmartin <nickdotmartin@googlemail.com>
2023-02-28 10:41:03 +0000

    exp1a_analysis_pipe_UVF_LVF.py: tried to run this on my sep4_5 data but it took 4 days and didn't finish.  I need to chop it into smaller chunks and save as I go.

--- Nick_scripts/exp1a_analysis_pipe_UVF_LVF.py
+++ Nick_scripts/exp1a_analysis_pipe_UVF_LVF.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from exp1a_psignifit_analysis import a_data_extraction, b3_plot_staircase, c_plots, \
     d_average_participant, e_average_exp_data, make_average_plots
-from psignifit_tools import get_psignifit_threshold_df
+from psignifit_tools import get_psignifit_threshold_df, get_psig_thr_w_hue
+from python_tools import which_path, running_on_laptop, switch_path
 
 '''
 This script if for checking for any differences between thr upper visual field and lower visual field.
@@ -18,19 +19,48 @@ Then run psignifit on this
 
 # # loop through run folders with first 4 scripts (a, get_psignifit_threshold_df, b3, c)
 # # then run script d to get master lists and averages
-exp_path = '/Users/nickmartin/Documents/PycharmProjects/Cardiff/exp1a_data'
-participant_list = ['aa', 'bb', 'cc', 'dd', 'ee']
+# exp_path = '/Users/nickmartin/Documents/PycharmProjects/Cardiff/exp1a_data'
+# participant_list = ['aa', 'bb', 'cc', 'dd', 'ee']
+# exp_path = '/Users/nickmartin/Documents/PycharmProjects/Cardiff/EXP1_sep4_5'
+exp_path = r"C:\Users\sapnm4\OneDrive - Cardiff University\PycharmProjects\Cardiff\EXP1_sep4_5"
+convert_path1 = os.path.normpath(exp_path)
+if running_on_laptop():
+    convert_path1 = switch_path(convert_path1, 'mac_oneDrive')
+exp_path = convert_path1
 
+participant_list = ['Nick']
 p_idx_plus = 1
 
+n_runs = 36
+analyse_from_run = 1
+trim_list = []
+
 for p_idx, participant_name in enumerate(participant_list):
-    root_path = f'{exp_path}/{participant_name}'
 
-    run_folder_names = [f'{participant_name}_1', f'{participant_name}_2',
-                        f'{participant_name}_3', f'{participant_name}_4',
-                        f'{participant_name}_5', f'{participant_name}_6']
+    root_path = os.path.join(exp_path, participant_name)
 
-    group_list = [1, 2]
+    # search to automatically get run_folder_names
+    dir_list = os.listdir(root_path)
+    run_folder_names = []
+    for i in range(n_runs):  # numbers 0 to 11
+        check_dir = f'{participant_name}_{i + analyse_from_run}'  # numbers 1 to 12
+        if check_dir in dir_list:
+            run_folder_names.append(check_dir)
+
+    if len(run_folder_names) > 0:
+        print("running analysis for:")
+        for i in run_folder_names:
+            print(i)
+    else:
+        print("no run folders found")
+    #
+    # root_path = f'{exp_path}/{participant_name}'
+    #
+    # run_folder_names = [f'{participant_name}_1', f'{participant_name}_2',
+    #                     f'{participant_name}_3', f'{participant_name}_4',
+    #                     f'{participant_name}_5', f'{participant_name}_6']
+    #
+    # group_list = [1, 2]
 
     # add RUNDATA-sorted to all_data
     all_data = []
@@ -55,7 +85,8 @@ for p_idx, participant_name in enumerate(participant_list):
     #     run_data_df = pd.read_excel(run_data_path, engine='openpyxl',
     #                                 usecols=['ISI',
     #                                          'stair',
-    #                                          'separation', 'group',
+    #                                          'separation',
+    #                                          # 'group',
     #                                          'probeLum', 'trial_response', 'corner'])
     #     print(f"run_data_df:\n{run_data_df}")
     #
@@ -72,131 +103,174 @@ for p_idx, participant_name in enumerate(participant_list):
     #
     # # create all_data_df - reshape to 2d
     # all_data_shape = np.shape(all_data)
-    # sheets, rows, columns = np.shape(all_data)
-    # all_data = np.reshape(all_data, newshape=(sheets * rows, columns))
-    # # if verbose:
-    # print(f'all_data reshaped from {all_data_shape} to {np.shape(all_data)}')
-    # all_data_df = pd.DataFrame(all_data, columns=column_names)
+    # print(f'all_data_shape:\n{all_data_shape}')
     #
+    # if len(np.shape(all_data)) == 2:
+    #     sheets, rows, columns = np.shape(all_data)
+    #     all_data = np.reshape(all_data, newshape=(sheets * rows, columns))
+    #     # if verbose:
+    #     print(f'all_data reshaped from {all_data_shape} to {np.shape(all_data)}')
+    #     all_data_df = pd.DataFrame(all_data, columns=column_names)
+    # else:
+    #     all_data_df = pd.concat(all_data, ignore_index=True)
+    #
+    # visual_field_list = ['UVF' if i < 200 else 'LVF' for i in all_data_df['corner'].to_list()]
+    # all_data_df['VF'] = visual_field_list
     # # if verbose:
     # print(f"all_data_df:\n{all_data_df}")
     #
     # # if save_all_data:
-    # save_name = 'P_all_runs_master_output.csv'
-    # save_csv_path = os.path.join(root_path, save_name)
+    save_name = 'P_all_runs_master_output.csv'
+    save_csv_path = os.path.join(root_path, save_name)
     # # if verbose:
     # print(f"\nsaving all_data_df to save_csv_path: {save_csv_path}")
     # all_data_df.to_csv(save_csv_path, index=False)
-    #
-    # all_data_df = pd.read_csv(os.path.join(root_path, 'P_all_runs_master_output.csv' ))
-    #
-    # UVF_df = all_data_df[all_data_df['corner'] < 200]
-    # LVF_df = all_data_df[all_data_df['corner'] > 200]
-    #
-    # vis_field_list = [UVF_df, LVF_df]
-    # vis_field_names = ['UVF', 'LVF']
-    #
-    # both_vfs = []
-    #
-    # for idx, vis_field_df in enumerate(vis_field_list):
-    #
-    #     vis_field_name = vis_field_names[idx]
-    #
-    #     print(f'Running psignifit for {vis_field_name}\n{vis_field_df}')
-    #
-    #
-    #     # stair_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-    #     sep_list = [0, 1, 2, 3, 6, 18, 99]
-    #     isi_list = [-1, 0, 2, 4, 6, 9, 12, 24]
-    #     cols_to_add_dict = {
-    #         # 'group': [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2],
-    #         #                 'separation': [18, 18, 6, 6, 3, 3, 2, 2, 1, 1, 0, 0, 20, 20],
-    #                         'vis_field': [vis_field_name, vis_field_name, vis_field_name, vis_field_name,
-    #                                       vis_field_name, vis_field_name, vis_field_name]}
-    #
-    #     '''get psignifit thresholds df - use stairs as sep levels rather than using groups'''
-    #     thr_df = get_psignifit_threshold_df(root_path=exp_path,
-    #                                         p_run_name=participant_name ,
-    #                                         csv_name=vis_field_df,
-    #                                         n_bins=10, q_bins=True,
-    #                                         sep_col='separation',
-    #                                         isi_list=isi_list,
-    #                                         sep_list=sep_list,
-    #                                         cols_to_add_dict=cols_to_add_dict,
-    #                                         save_name=f'psignifit_{vis_field_name}',
-    #                                         verbose=True)
-    #     print(f'psignifit_{vis_field_name}:\n{thr_df}')
-    #     column_names = list(thr_df)
-    #
-    #     both_vfs.append(thr_df)
-    #
-    # # create all_data_df - reshape to 2d
-    # both_vfs_shape = np.shape(both_vfs)
-    # sheets, rows, columns = np.shape(both_vfs)
-    # both_vfs = np.reshape(both_vfs, newshape=(sheets * rows, columns))
-    # print(f'both_vfs reshaped from {both_vfs_shape} to {np.shape(both_vfs)}')
-    # both_vfs_df = pd.DataFrame(both_vfs, columns=column_names)
-    # print(f"both_vfs_df:\n{both_vfs_df}")
-    #
-    # save_name = 'psignifit_both_vfs.csv'
-    # save_csv_path = os.path.join(root_path, save_name)
-    # print(f"\nsaving all_data_df to save_csv_path:\n{save_csv_path}")
-    # both_vfs_df.to_csv(save_csv_path, index=False)
 
-    psig_both_vf_df = pd.read_csv(os.path.join(root_path, 'psignifit_both_vfs.csv'))
-    # sep_values = psig_both_vf_df['separation'].to_list()
-    # sep_values = [20 if i == 99 else i for i in sep_values]
-    # sep_labels = ['1pr' if i == 20 else i for i in sep_values]
-    # psig_both_vf_df['separation'] = sep_values
-    cond_vals = [0, 1, 2, 3, 6, 18, 20,
-                 -.1, -1, -2, -3, -6, -18, -20]
-    cond_labels = [0, 1, 2, 3, 6, 18, '1pr',
-                   -.1, -1, -2, -3, -6, -18, '-1pr']
-    psig_both_vf_df.insert(0, 'cond', cond_vals)
-    psig_both_vf_df.sort_values(by='cond', inplace=True)
-    psig_both_vf_df.set_index('cond', drop=True, inplace=True)
-    print(f"psig_both_vf_df:\n{psig_both_vf_df}")
-    psig_both_vf_df = psig_both_vf_df.drop(['separation', 'vis_field'], axis=1)
-    print(f"psig_both_vf_df:\n{psig_both_vf_df}")
+    all_data_df = pd.read_csv(os.path.join(root_path, 'P_all_runs_master_output.csv' ))
 
-    # make plot to compare UVF and LVF
-    fig_title = f'{participant_name} compare UVF & LVF'
-    legend_title = 'ISI'
-    x_tick_vals = cond_vals
-    x_tick_labels = cond_labels
-    x_axis = 'Sep in diag pixels. Neg=LVF, Pos=UVF'
-    y_axis = 'Threshold'
-    log_x = False
-    log_y = False
-    save_as = os.path.join(root_path, 'compare_vfs.png')
+    psig_save_name = 'psignifit_both_vfs.csv'
 
-    fig, ax = plt.subplots(figsize=(10, 6))
-    sns.lineplot(data=psig_both_vf_df,
-                 markers=True, dashes=False, ax=ax)
-    if fig_title is not None:
-        plt.title(fig_title)
-    if legend_title is not None:
-        plt.legend(title=legend_title)
-    if x_tick_vals is not None:
-        ax.set_xticks(x_tick_vals)
-    if x_tick_labels is not None:
-        ax.set_xticklabels(x_tick_labels)
-        if -18 in x_tick_labels:
-            # add dotted line at zero
-            ax.axvline(x=0, linestyle="-.", color='lightgrey')
-    if log_x:
-        ax.set(xscale="log")
-        x_axis = f'log {x_axis}'
-    if log_y:
-        ax.set(yscale="log")
-        y_axis = f'log {y_axis}'
-    if x_axis is not None:
-        ax.set_xlabel(x_axis)
-    if y_axis is not None:
-        ax.set_ylabel(y_axis)
-    if save_as is not None:
-        plt.savefig(save_as)
-    plt.show()
+    # sep_list = sorted(all_data_df['separation'].to_list())
+    # isi_list = sorted(all_data_df['ISI'].to_list())
+    # hue_list = sorted(all_data_df['VF'].to_list())
+    #
+    # get_psig_thr_w_hue(root_path, p_run_name=participant_name,
+    #                    output_df=all_data_df,
+    #                    n_bins=25, q_bins=True,
+    #                    thr_col='probeLum',
+    #                    sep_col='separation', sep_list=sep_list,
+    #                    isi_col='ISI', isi_list=isi_list,
+    #                    hue_col='VF', hue_list=hue_list,
+    #                    trial_correct_col='trial_response',
+    #                    conf_int=True, thr_type='Bayes',
+    #                    plot_both_curves=False,
+    #                    cols_to_add_dict=None, save_name=psig_save_name,
+    #                    show_plots=False, save_plots=False,
+    #                    verbose=True)
+
+
+    UVF_df = all_data_df[all_data_df['corner'] < 200]
+    LVF_df = all_data_df[all_data_df['corner'] > 200]
+
+    UVF_df.to_csv(save_csv_path, index=False)
+
+
+    vis_field_list = [UVF_df, LVF_df]
+    vis_field_names = ['UVF', 'LVF']
+
+    both_vfs = []
+
+    for idx, vis_field_df in enumerate(vis_field_list):
+
+        vis_field_name = vis_field_names[idx]
+
+        print(f'Running psignifit for {vis_field_name}\n{vis_field_df}')
+
+
+        # stair_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+        # sep_list = [0, 1, 2, 3, 6, 18, 99]
+        sep_list = sorted(vis_field_df['separation'].to_list())
+        # isi_list = [-1, 0, 2, 4, 6, 9, 12, 24]
+        isi_list = sorted(vis_field_df['ISI'].to_list())
+
+        # cols_to_add_dict = {
+        #     # 'group': [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2],
+        #     #                 'separation': [18, 18, 6, 6, 3, 3, 2, 2, 1, 1, 0, 0, 20, 20],
+        #                     'vis_field': [vis_field_name, vis_field_name, vis_field_name, vis_field_name,
+        #                                   vis_field_name, vis_field_name, vis_field_name]}
+
+        '''get psignifit thresholds df - use stairs as sep levels rather than using groups'''
+
+        thr_df = get_psignifit_threshold_df(root_path=exp_path,
+                                            p_run_name=participant_name,
+                                            csv_name=vis_field_df,
+                                            n_bins=9, q_bins=True,
+                                            thr_col='probeLum',
+                                            sep_col='separation', sep_list=sep_list,
+                                            isi_col='ISI', isi_list=isi_list,
+                                            conf_int=True, thr_type='Bayes',
+                                            plot_both_curves=False,
+                                            cols_to_add_dict=None, save_name=f'psignifit_{vis_field_name}',
+                                            show_plots=False, save_plots=False,
+                                            verbose=True)
+
+        thr_df['VF'] = vis_field_name
+
+        print(f'psignifit_{vis_field_name}:\n{thr_df}')
+        column_names = list(thr_df)
+
+        both_vfs.append(thr_df)
+
+    # create all_data_df - reshape to 2d
+    both_vfs_shape = np.shape(both_vfs)
+    sheets, rows, columns = np.shape(both_vfs)
+    both_vfs = np.reshape(both_vfs, newshape=(sheets * rows, columns))
+    print(f'both_vfs reshaped from {both_vfs_shape} to {np.shape(both_vfs)}')
+    both_vfs_df = pd.DataFrame(both_vfs, columns=column_names)
+    print(f"both_vfs_df:\n{both_vfs_df}")
+
+    save_name = 'psignifit_both_vfs.csv'
+    save_csv_path = os.path.join(root_path, save_name)
+    print(f"\nsaving all_data_df to save_csv_path:\n{save_csv_path}")
+    both_vfs_df.to_csv(save_csv_path, index=False)
+    #
+    # psig_both_vf_df = pd.read_csv(os.path.join(root_path, 'psignifit_both_vfs.csv'))
+    # # sep_values = psig_both_vf_df['separation'].to_list()
+    # # sep_values = [20 if i == 99 else i for i in sep_values]
+    # # sep_labels = ['1pr' if i == 20 else i for i in sep_values]
+    # # psig_both_vf_df['separation'] = sep_values
+    #
+    #
+    #
+    # cond_vals = [0, 1, 2, 3, 6, 18, 20,
+    #              -.1, -1, -2, -3, -6, -18, -20]
+    # cond_labels = [0, 1, 2, 3, 6, 18, '1pr',
+    #                -.1, -1, -2, -3, -6, -18, '-1pr']
+    # psig_both_vf_df.insert(0, 'cond', cond_vals)
+    # psig_both_vf_df.sort_values(by='cond', inplace=True)
+    # psig_both_vf_df.set_index('cond', drop=True, inplace=True)
+    # print(f"psig_both_vf_df:\n{psig_both_vf_df}")
+    # psig_both_vf_df = psig_both_vf_df.drop(['separation', 'vis_field'], axis=1)
+    # print(f"psig_both_vf_df:\n{psig_both_vf_df}")
+    #
+    # # make plot to compare UVF and LVF
+    # fig_title = f'{participant_name} compare UVF & LVF'
+    # legend_title = 'ISI'
+    # x_tick_vals = cond_vals
+    # x_tick_labels = cond_labels
+    # x_axis = 'Sep in diag pixels. Neg=LVF, Pos=UVF'
+    # y_axis = 'Threshold'
+    # log_x = False
+    # log_y = False
+    # save_as = os.path.join(root_path, 'compare_vfs.png')
+    #
+    # fig, ax = plt.subplots(figsize=(10, 6))
+    # sns.lineplot(data=psig_both_vf_df,
+    #              markers=True, dashes=False, ax=ax)
+    # if fig_title is not None:
+    #     plt.title(fig_title)
+    # if legend_title is not None:
+    #     plt.legend(title=legend_title)
+    # if x_tick_vals is not None:
+    #     ax.set_xticks(x_tick_vals)
+    # if x_tick_labels is not None:
+    #     ax.set_xticklabels(x_tick_labels)
+    #     if -18 in x_tick_labels:
+    #         # add dotted line at zero
+    #         ax.axvline(x=0, linestyle="-.", color='lightgrey')
+    # if log_x:
+    #     ax.set(xscale="log")
+    #     x_axis = f'log {x_axis}'
+    # if log_y:
+    #     ax.set(yscale="log")
+    #     y_axis = f'log {y_axis}'
+    # if x_axis is not None:
+    #     ax.set_xlabel(x_axis)
+    # if y_axis is not None:
+    #     ax.set_ylabel(y_axis)
+    # if save_as is not None:
+    #     plt.savefig(save_as)
+    # plt.show()
 
 
 
